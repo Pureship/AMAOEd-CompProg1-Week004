20df0ec10529e5d9c2ea4e5857b40f129edc772b
Pureship <132271332+Pureship@users.noreply.github.com>
2024-10-31 09:47:06 +0800

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+
+int main() {
+    
+    cout << "Hello" << endl;
+    cout << "World" << endl;
+
+    cout << "Hello" << endl;
+    cout << "World" << endl;
+    cout << "Hello" << endl;
+    cout << "World" << endl;
+    cout << "Hello" << endl;
+    cout << "World" << endl;
+    cout << "Hello" << endl;
+    cout << "World" << endl;
+    cout << "Hello" << endl;
+    cout << "World" << endl;
+
+    cout << "Hello" << endl;
+    cout << "World" << endl;
+    cout << "World" << endl;
+    cout << "World" << endl;
+    cout << "World" << endl;
+    cout << "World" << endl;
+
+    system ("pause");
+    return 0;
+}
+    
