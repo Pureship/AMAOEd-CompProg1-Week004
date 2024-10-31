354cb058ce3c13e4b327590b7fec0856ce00ca83
Pureship <132271332+Pureship@users.noreply.github.com>
2024-10-31 09:51:03 +0800

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,28 @@ int main() {
     cout << "Hello" << endl;
     cout << "World" << endl;
 
-    cout << "Hello" << endl;
-    cout << "World" << endl;
-    cout << "Hello" << endl;
-    cout << "World" << endl;
-    cout << "Hello" << endl;
-    cout << "World" << endl;
-    cout << "Hello" << endl;
-    cout << "World" << endl;
-    cout << "Hello" << endl;
-    cout << "World" << endl;
+    cout << "H";
+    cout << "e";
+    cout << "l";
+    cout << "l";
+    cout << "o" << endl;
 
-    cout << "Hello" << endl;
-    cout << "World" << endl;
-    cout << "World" << endl;
-    cout << "World" << endl;
-    cout << "World" << endl;
-    cout << "World" << endl;
+    cout << "W";
+    cout << "o";
+    cout << "r";
+    cout << "l";
+    cout << "d" << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Hello"[i];
+    }
+    cout << endl;
+
+    cout << "W";
+    cout << "o";
+    cout << "r";
+    cout << "l";
+    cout << "d" << endl;
 
     system ("pause");
     return 0;
